91a5a24d724fef64f0c5e3430736302b9c7052a2
Haxxxxxx <62444678+Haxxxxxx@users.noreply.github.com>
2023-06-04 16:46:48 +0200

    maj update

--- functions/index.js
+++ functions/index.js
@@ -1,70 +1,38 @@
-const functions = require("firebase-functions");
-const admin = require("firebase-admin");
-const nodemailer = require("nodemailer");
-const express = require("express");
-const cors = require("cors");
-const {logger} = require("firebase-functions");
-const {onRequest} = require("firebase-functions/v2/https");
-const {onDocumentCreated} = require("firebase-functions/v2/firestore");
+const functions = require('firebase-functions');
+const nodemailer = require('nodemailer');
+const cors = require('cors')({ origin: true });
 
-// The Firebase Admin SDK to access Firestore.
-const {initializeApp} = require("firebase-admin/app");
-const {getFirestore} = require("firebase-admin/firestore");
+exports.sendEmailToUser = functions.firestore
+  .document('contacts/{contactId}')
+  .onCreate((snapshot, context) => {
+    const contactData = snapshot.data();
 
-initializeApp();
-const app = express();
-app.use(cors());
-app.use(express.json());
+    // Configure the email transport
+    const transporter = nodemailer.createTransport({
+      service: 'Gmail',
+      auth: {
+        user: 'theiaweb.contact@gmail.com',
+        pass: 'cwsmnseyqepnqplf'
+      }
+    });
 
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: "theiaweb.contact@gmail.com",
-    pass: "GC4*EVER!",
-  },
-});
+    const mailOptions = {
+      from: 'theiaweb.contact@gmail.com',
+      to: contactData.email,
+      subject: 'Thank you for contacting us',
+      text: `Dear ${contactData.name},\n\nThank you for reaching out to us. We have received your message and will get back to you as soon as possible.\n\nBest regards,\nYour Company`,
+      html: `<p>Dear ${contactData.name},</p>
+        <p>Thank you for reaching out to us. We have received your message and will get back to you as soon as possible.</p>
+        <p>Best regards,<br>Your Company</p>`
+    };
 
-app.post(
-  "/formtheia/us-central1/sendMailOverHTTP",
-  async (req, res) => {
-    try {
-      const { name, email, phone, company, subject, message } = req.body;
-
-      // Construct the message
-      const mailOptions = {
-        from: "theiaweb.contact@gmail.com",
-        to: email,
-        subject: `${company} represented by ${name} : ${subject}`,
-        html: `<p>Name: ${name}</p> 
-              <p>Email: ${email}</p> 
-              <p>Phone: ${phone}</p> 
-              <p>Company: ${company}</p> 
-              <p>Message: ${message}</p>`,
-      };
-
-      // Send the email
-      await transporter.sendMail(mailOptions);
-
-      // Send the response only after the email has been sent
-      res.status(200).send("Email sent successfully");
-    } catch (error) {
-      console.error("Error sending email:", error);
-      res.status(500).send("Error sending email");
-    }
-  }
-);
-// Configure CORS options
-const corsOptions = {
-  origin: true, // Allow all origins
-};
-
-// Enable CORS for the Cloud Function
-app.use(cors(corsOptions));
-exports.testEndpoint = functions.https.onRequest((req, res) => {
-  res.send('Test endpoint');
-});
-
-// Export the Cloud Function
-exports.sendMailOverHTTP = functions.https.onRequest(app);
-
-
+    return transporter.sendMail(mailOptions)
+      .then(() => {
+        console.log('Email sent to user');
+        return null;
+      })
+      .catch((error) => {
+        console.error('Error sending email to user:', error);
+        return null;
+      });
+  });
