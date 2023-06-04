00d77b344942addb350521438b2595b18fe53ea8
Haxxxxxx <62444678+Haxxxxxx@users.noreply.github.com>
2023-06-04 16:17:50 +0200

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,11 @@
-const functions = require("firebase-functions");
-const admin = require("firebase-admin");
 const nodemailer = require("nodemailer");
 const express = require("express");
 const cors = require("cors");
-const {logger} = require("firebase-functions");
-const {onRequest} = require("firebase-functions/v2/https");
-const {onDocumentCreated} = require("firebase-functions/v2/firestore");
+const { logger } = require("firebase-functions");
+const { https, firestore } = require("firebase-functions");
 
-// The Firebase Admin SDK to access Firestore.
-const {initializeApp} = require("firebase-admin/app");
-const {getFirestore} = require("firebase-admin/firestore");
+const { initializeApp } = require("firebase-admin");
+const { getFirestore } = require("firebase-admin/firestore");
 
 const firebaseConfig = {
   apiKey: "AIzaSyDB4BfdCWo9fHb4rC2YZl5gOgtikxQHi5g",
@@ -34,83 +30,57 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-app.post("/formtheia/us-central1/sendMailOverHTTP", async (req, res) => {
-  try {
-    const { name, email, phone, company, subject, message } = req.body;
+app.post(
+  "/formtheia/us-central1/sendMailOverHTTP",
+  async (req, res) => {
+    try {
+      const { name, email, phone, company, subject, message } = req.body;
 
-    // Construct the message
-    const mailOptions = {
-      from: "theiaweb.contact@gmail.com",
-      to: email,
-      subject: `${company} represented by ${name} : ${subject}`,
-      html: `<p>Name: ${name}</p> <p>Email: ${email}</p> <p>Phone: ${phone}</p> <p>Company: ${company}</p> <p>Message: ${message}</p>`,
-    };
+      // Construct the message
+      const mailOptions = {
+        from: "theiaweb.contact@gmail.com",
+        to: email,
+        subject: `${company} represented by ${name} : ${subject}`,
+        html: `<p>Name: ${name}</p> <p>Email: ${email}</p> <p>Phone: ${phone}</p> <p>Company: ${company}</p> <p>Message: ${message}</p>`,
+      };
 
-    // Send the email
-    await transporter.sendMail(mailOptions);
+      // Send the email
+      await transporter.sendMail(mailOptions);
 
-    // Send the response only after the email has been sent
-    res.status(200).send("Email sent successfully");
-  } catch (error) {
-    console.error("Error sending email:", error);
-    res.status(500).send("Error sending email");
+      // Send the response only after the email has been sent
+      res.status(200).send("Email sent successfully");
+    } catch (error) {
+      console.error("Error sending email:", error);
+      res.status(500).send("Error sending email");
+    }
   }
-});
+);
 
-// Configure CORS options
 const corsOptions = {
   origin: true, // Allow all origins
 };
 
-// Enable CORS for the Cloud Function
 app.use(cors(corsOptions));
-exports.testEndpoint = functions.https.onRequest((req, res) => {
-  res.send('Test endpoint');
+
+exports.testEndpoint = https.onRequest((req, res) => {
+  res.send("Test endpoint");
 });
 
-// Export the Cloud Function
-exports.sendMailOverHTTP = functions.https.onRequest(app);
+exports.sendMailOverHTTP = https.onRequest(app);
 
-
-// The Cloud Functions for Firebase SDK to create Cloud Functions and triggers.
-const {logger} = require("firebase-functions");
-const {onRequest} = require("firebase-functions/v2/https");
-const {onDocumentCreated} = require("firebase-functions/v2/firestore");
-
-// The Firebase Admin SDK to access Firestore.
-const {initializeApp} = require("firebase-admin/app");
-const {getFirestore} = require("firebase-admin/firestore");
-
-initializeApp();
-
-// Take the text parameter passed to this HTTP endpoint and insert it into
-// Firestore under the path /messages/:documentId/original
-exports.addmessage = onRequest(async (req, res) => {
-  // Grab the text parameter.
+exports.addmessage = https.onRequest(async (req, res) => {
   const original = req.query.text;
-  // Push the new message into Firestore using the Firebase Admin SDK.
   const writeResult = await getFirestore()
-      .collection("messages")
-      .add({original: original});
-  // Send back a message that we've successfully written the message
-  res.json({result: `Message with ID: ${writeResult.id} added.`});
+    .collection("messages")
+    .add({ original: original });
+  res.json({ result: `Message with ID: ${writeResult.id} added.` });
 });
 
-// Listens for new messages added to /messages/:documentId/original
-// and saves an uppercased version of the message
-// to /messages/:documentId/uppercase
-exports.makeuppercase = onDocumentCreated("/messages/{documentId}", (event) => {
-  // Grab the current value of what was written to Firestore.
-  const original = event.data.data().original;
-
-  // Access the parameter `{documentId}` with `event.params`
-  logger.log("Uppercasing", event.params.documentId, original);
-
-  const uppercase = original.toUpperCase();
-
-  // You must return a Promise when performing
-  // asynchronous tasks inside a function
-  // such as writing to Firestore.
-  // Setting an 'uppercase' field in Firestore document returns a Promise.
-  return event.data.ref.set({uppercase}, {merge: true});
-});index2.js
+exports.makeuppercase = firestore
+  .document("/messages/{documentId}")
+  .onCreate((snap, context) => {
+    const original = snap.data().original;
+    logger.log("Uppercasing", context.params.documentId, original);
+    const uppercase = original.toUpperCase();
+    return snap.ref.set({ uppercase }, { merge: true });
+});
